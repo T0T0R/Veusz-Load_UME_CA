1683114acb9fc96c2d7a05ec1168a8b3f2d7c9a5
T0T0R <arthur.electron@orange.fr>
2023-02-07 20:14:08 +0100

    Split current dataset according to the mask dataset.

--- Veusz-Load_UME_CA.py
+++ Veusz-Load_UME_CA.py
@@ -38,16 +38,16 @@ class LoadUMEfilesPlugin(plugins.ToolsPlugin):
     name = 'Load UME files'
 
     # name to appear on status tool bar
-    description_short = 'Load files related to an experiment of vesicles nano-impacts at a ultramicroelectrode.'
+    description_short = 'Load files related to an experiment of vesicles nano-impacts at an ultramicroelectrode.'
     # text to appear in dialog box
-    description_full = 'Load files related to an experiment of vesicles nano-impacts at a ultramicroelectrode.'
+    description_full = 'Load files related to an experiment of vesicles nano-impacts at an ultramicroelectrode.'
 
     def __init__(self):
         """Make list of fields."""
         self.fields = [ 
             plugins.FieldFilename("filename_start", descr="First file"),
             plugins.FieldInt("nb_files", descr="Number of files", default=1, minval=1),
-            plugins.FieldInt("spread_size", descr="Width of current change", default=10, minval=1),
+            plugins.FieldInt("spread_size", descr="Width of current change", default=10, minval=4),
             ]
 
     def apply(self, interface, fields):
@@ -115,11 +115,67 @@ class LoadUMEfilesPlugin(plugins.ToolsPlugin):
             interface.Set('yData', filename_root + f"{i + start_no:02d}" + "_<I>/mA")
             interface.To('..')
 
+            self.create_I_masked_plots(
+                                        interface,
+                                        filename_root + f"{i + start_no:02d}" + "_<I>/mA",
+                                        filename_root + f"{i + start_no:02d}" + "_time/s",
+                                        filename_root + f"{i + start_no:02d}" + "_I Range_change_M")
+
+
     
+
+    def create_I_masked_plots(self, interface, dataset_I_full_str, dataset_t_full_str, dataset_I_mask_full_str,):
+        dataset_I_full = interface.GetData(dataset_I_full_str)[0]
+        dataset_t_full = interface.GetData(dataset_t_full_str)[0]
+        # The mask is longer than the dataset, so trim it at the end:
+        dataset_I_mask_full = interface.GetData(dataset_I_mask_full_str)[0][:len(dataset_I_full)]
+
+        dataset_It_full = numpy.column_stack((dataset_I_full, dataset_t_full, dataset_I_mask_full))
+        datasets_It_list = []
+        # [(I0,t0,m0)   [(I0,t0,m0)    [(I0,t0,m0)
+        #  (I1,t1,m1)    (I1,t1,m1)],   (I1,t1,m1)
+        #  (I2,t2,m2)                   (I2,t2,m2)]
+        #  (I3,t3,m3)],
+
+        
+
+        dataset_It = []
+        temp_dataset = True
+        for data_slice in dataset_It_full:          #   For every value of current/time
+            if data_slice[2] == 1:                  # if it is not masked
+                dataset_It.append(data_slice[:2])   # add it to the temporary dataset.
+                temp_dataset = True
+            elif temp_dataset:                                          #   If it is masked, the temporary dataset
+                if len(dataset_It) > 0:
+                    datasets_It_list.append(numpy.stack(dataset_It))  # is done and added to the list of datasets.
+                dataset_It = []                                         # The temporary dataset is reset.
+                temp_dataset = False
+        
+        if len(dataset_It) > 0:                     # If the last values have been checked without
+            datasets_It_list.append(numpy.stack(dataset_It))     # being masked, the temporary dataset is added.
+        
+        #datasets_It_list = numpy.asarray(datasets_It_list) # Convert a list of list of tuples (I,t) into numpy 3D-array
+
+
+        for no_dataset, dataset_It in enumerate(datasets_It_list):
+            dataset_I = dataset_It[:,0]
+            dataset_t = dataset_It[:,1]
+            interface.SetData(dataset_I_full_str + "_" + str(no_dataset), dataset_I, symerr=None, negerr=None, poserr=None)
+            interface.SetData(dataset_t_full_str + "_" + str(no_dataset), dataset_t, symerr=None, negerr=None, poserr=None)
+
+
+
 
 
 
     def create_I_change_dataset(self, interface, I_range_dataset_str, spread_size=10):
+        """Create a dataset which can be used as a mask to hide current values
+        when the current range changes.
+        I_range_dataset_str is the name of the dataset containing the current ranges.
+        spread_size is the number of values to be masked after a current range change.
+        Return None.
+        """
+
         I_range_dataset_np = interface.GetData(I_range_dataset_str)[0]
 
         # Converts [56,56,56,57,57,57,57,58] to [0,0,0,1,0,0,0,1]
@@ -149,6 +205,7 @@ class LoadUMEfilesPlugin(plugins.ToolsPlugin):
 
         interface.SetData(I_range_dataset_str + "_change_M", numpy.logical_not(numpy.ma.make_mask(I_change_dataset_spread)), symerr=None, negerr=None, poserr=None)
 
+    
 
 
 plugins.toolspluginregistry.append(LoadUMEfilesPlugin)
